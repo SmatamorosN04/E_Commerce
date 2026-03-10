ef1cc091afc037eed90dd905f11c823aceffa69e
Sergio Matamoros <124097535+SmatamorosN04@users.noreply.github.com>
2026-03-10 14:07:03 -0600

    Database added

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-/// <reference types="vitest" />
 
 import { defineConfig } from 'vite';
 import analog from '@analogjs/platform';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   build: {
     target: ['es2020'],
